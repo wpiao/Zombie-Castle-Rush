39aa1b2a63ebf628435e7736783d2771724ae724
gtulkn12 <gtulkn1@yahoo.com>
2021-06-03 18:12:16 -0400

    AB#193 removed main from Parser

--- src/com/zombiecastlerush/util/Parser.java
+++ src/com/zombiecastlerush/util/Parser.java
@@ -5,21 +5,17 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
-    public static void main(String[] args) throws Exception {
-        new Parser().parse("Go North");
-    }
-
     public static List<String> parse(String input){
         List<String> actions = new ArrayList<>(Arrays.asList("go", "look", "pick-up", "drop"));
         List<String> commands = new ArrayList<>(Arrays.asList(input.toLowerCase().split(" ")));
-        System.out.println(actions.contains(commands.get(0)) ? reduceArray((ArrayList<String>) commands) : null);
-        return actions.contains(commands.get(0)) ? reduceArray((ArrayList<String>) commands) : null;
+        System.out.println(actions.contains(commands.get(0)) ? reduceArray(commands) : null);
+        return actions.contains(commands.get(0)) ? reduceArray(commands) : null;
     }
 
-    public static ArrayList<String> reduceArray(ArrayList<String>  arr){
-        List<String> list = new ArrayList<String>(arr);
+    public static List<String> reduceArray(List<String>  arr){
+        List<String> list = new ArrayList<>(arr);
         list.removeAll(Arrays.asList("", null, " "));  //removes empty and null elements
         list.replaceAll(x -> x.trim());
-        return (ArrayList<String>) list;
+        return list;
     }
 }
