9628545ce0c219581cecce7ce789e1fd754b5dda
Chaynor Hsiao <chaynsaw@gmail.com>
2021-06-03 21:39:47 -0700

    AB#194 Add capitalization variant tests, remove new ArrayList<>() constructor, as no ArrayList specific methods are needed

--- test/com/zombiecastlerush/util/ParserTest.java
+++ test/com/zombiecastlerush/util/ParserTest.java
@@ -2,18 +2,24 @@ package com.zombiecastlerush.util;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ParserTest {
 
     @Test
     public void parseAcceptsTwoWordStringReturnsArrayListOfTwoStrings() {
-        List<String> expected = new ArrayList<>(Arrays.asList("go", "north"));
+        List<String> expected = Arrays.asList("go", "north");
         List<String> actual = Parser.parse("go north");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void parseAcceptsAnyCapitalizationVariantOfInput() {
+        List<String> expected = Arrays.asList("go", "north");
+        List<String> actual = Parser.parse("Go North");
         assertEquals(expected, actual);
     }
 }
