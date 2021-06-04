09fd5ef05c77859998e72ff84d9f9651040fd2fd
Chaynor Hsiao <chaynsaw@gmail.com>
2021-06-03 21:24:32 -0700

    AB#194 Change actions to ALLOWED_ACTIONS constant, rename commands -> inputWords, and rename reduceArray() to reduceInputWordsToList()

--- src/com/zombiecastlerush/util/Parser.java
+++ src/com/zombiecastlerush/util/Parser.java
@@ -5,14 +5,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
+
+    final static List<String> ALLOWED_ACTIONS = new ArrayList<>(Arrays.asList("go", "look", "pick-up", "drop"));
+
     public static List<String> parse(String input){
-        List<String> actions = new ArrayList<>(Arrays.asList("go", "look", "pick-up", "drop"));
-        List<String> commands = new ArrayList<>(Arrays.asList(input.toLowerCase().split(" ")));
-        System.out.println(actions.contains(commands.get(0)) ? reduceArray(commands) : null);
-        return actions.contains(commands.get(0)) ? reduceArray(commands) : null;
+        List<String> inputWords = new ArrayList<>(Arrays.asList(input.toLowerCase().split(" ")));
+        System.out.println(ALLOWED_ACTIONS.contains(inputWords.get(0)) ? reduceInputWordsToList(inputWords) : null);
+        return ALLOWED_ACTIONS.contains(inputWords.get(0)) ? reduceInputWordsToList(inputWords) : null;
     }
 
-    public static List<String> reduceArray(List<String>  arr){
+    public static List<String> reduceInputWordsToList(List<String>  arr){
         List<String> list = new ArrayList<>(arr);
         list.removeAll(Arrays.asList("", null, " "));  //removes empty and null elements
         list.replaceAll(x -> x.trim());
