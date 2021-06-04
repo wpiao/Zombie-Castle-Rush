bdf6f7e157255a19ad271300a714cacf309d2ff4
wang.xander <wang.xander@gmail.com>
2021-06-04 07:46:54 -0700

    AB#198 Updated moveTo() returns movement sucessfull or not

--- src/com/zombiecastlerush/role/Player.java
+++ src/com/zombiecastlerush/role/Player.java
@@ -21,14 +21,16 @@ class Player extends Role{
      * TODO: move to a room by room name/description
      * @param roomName
      */
-    public void moveTo(String roomName){
+    public boolean moveTo(String roomName){
         Room targetRoom = this.canMoveToRoom(roomName);
         if(targetRoom != null){
             String previous = super.getCurrentPosition().getName();
             super.setCurrentPosition(targetRoom);
             System.out.printf("Player %s moved from room %s to room %s\n", super.getName(), previous, super.getCurrentPosition().getName());
+            return true;
         } else{
             System.out.printf("Player %s's current room %s doesn't connect to other rooms.", super.getName(), super.getCurrentPosition().getName());
+            return false;
         }
     }
 
