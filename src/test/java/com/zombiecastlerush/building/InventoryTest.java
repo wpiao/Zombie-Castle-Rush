e45e210051381f276517c7618352d7d733ef967b
gtulkn12 <gtulkn1@yahoo.com>
2021-06-07 15:47:45 -0400

    AB#274 created inventoryTest

---
+++ src/test/java/com/zombiecastlerush/building/InventoryTest.java
@@ -0,0 +1,25 @@
+package com.zombiecastlerush.building;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InventoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getItems() {
+    }
+
+    @Test
+    public void addItems() {
+    }
+
+    @Test
+    public void deleteItems() {
+    }
+}
