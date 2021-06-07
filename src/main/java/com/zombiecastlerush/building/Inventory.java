b95fb00a267e5c47e5bc412117339b75d1736a11
gtulkn12 <gtulkn1@yahoo.com>
2021-06-07 15:43:45 -0400

    AB#274 created inventory class

--- src/main/java/com/zombiecastlerush/building/Inventory.java
+++ src/main/java/com/zombiecastlerush/building/Inventory.java
@@ -1,7 +1,27 @@
 package com.zombiecastlerush.building;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * TODO: a Shop, a Room or a Player can HAS-A Inventory
  */
-class Inventory {
+public class Inventory {
+    private List<Item> items = new ArrayList<>();
+
+    public Inventory(Item item){
+        addItems(item);
+    }
+
+    public List<Item> getItems(){
+        return items;
+    }
+
+    public void addItems(Item item){
+
+    }
+
+    public void deleteItems(Item item){
+        items.remove(item);
+    }
 }
