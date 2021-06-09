d35fabca7b5c1fb38cc6bbd1783268d73c0ea96a
wang.xander <wang.xander@gmail.com>
2021-06-09 07:32:54 -0700

    AB#314 Updated Inventory Test for move a item between objects

--- src/test/java/com/zombiecastlerush/building/InventoryTest.java
+++ src/test/java/com/zombiecastlerush/building/InventoryTest.java
@@ -1,6 +1,7 @@
 package com.zombiecastlerush.building;
 
 import com.zombiecastlerush.role.Player;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -32,5 +33,63 @@ public class InventoryTest {
 
     @Test
     public void deleteItems() {
+    }
+
+    @Test
+    public void testMoveItem_targetItemInDestinationInventoryAfterMovement(){
+        Player player = new Player("Player");
+        Room room = new Room("Room", "just a room");
+        //TODO: need to test Puzzle inventory once updated version available
+        Item item = new Item("target item", "I will be moved between objects");
+        player.getInventory().addItems(item);
+        Assert.assertEquals(room.getInventory().getItems().size(), 0); //empty inventory
+        Assert.assertEquals(player.getInventory().getItems().get(0).getName(),item.getName()); //one item in inventory
+        // move item from player to room
+        player.getInventory().moveItem(item,room);
+        Assert.assertEquals(player.getInventory().getItems().size(), 0); //empty
+        Assert.assertEquals(room.getInventory().getItems().get(0).getName(), item.getName()); //one item
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMoveItem_throwExceptionTargetItemNull(){
+        Player player = new Player("Player");
+        Room room = new Room("Room", "just a room");
+        Item item = null;
+        // move item null from player to room
+        player.getInventory().moveItem(item,room);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMoveItem_throwExceptionDestinationObjectNull(){
+        Player player = new Player("Player");
+        Room room = null;
+        Item item = new Item("target item", "I will be moved between objects");
+        player.getInventory().getItems().add(item);
+        Assert.assertEquals(player.getInventory().getItems().get(0).getName(),item.getName());
+        //can not move item to null
+        player.getInventory().moveItem(item,room);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMoveItem_throwExceptionMovingNonexistentItem(){
+        Player player = new Player("Player");
+        Room room = new Room("Room", "just a room");
+        Item item = new Item("target item", "I will be moved between objects");
+        Item nonExistentItem = new Item("nonexistent item", "i don't belong to you and you can not move me");
+        player.getInventory().getItems().add(item);
+        Assert.assertEquals(player.getInventory().getItems().get(0).getName(),item.getName());
+        // item doesn't belong to player, so cannot be moved
+        player.getInventory().moveItem(nonExistentItem,room);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMoveItem_throwExceptionUnrecognizedDestination(){
+        Player player = new Player("Player");
+        Castle castle = new Castle();
+        Item item = new Item("target item", "I will be moved between objects");
+        player.getInventory().getItems().add(item);
+        Assert.assertEquals(player.getInventory().getItems().get(0).getName(),item.getName()); // i have the item
+        // move to a destination that can not be recognized
+        player.getInventory().moveItem(item, castle);
     }
 }
