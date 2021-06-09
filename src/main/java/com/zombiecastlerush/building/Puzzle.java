e259a60ab322cfa5b1de42a5ec0bb71e1427d20b
kaashis <aashiskarki@my.unt.edu>
2021-06-09 08:53:26 -0500

    simplied puzzle class to 1 Q&A, modified attempt puzzle to return an int, added test classes for puzzle and room AB#77 #21

--- src/main/java/com/zombiecastlerush/building/Puzzle.java
+++ src/main/java/com/zombiecastlerush/building/Puzzle.java
@@ -1,82 +1,59 @@
 package com.zombiecastlerush.building;
 
-import java.util.*;
-
 class Puzzle extends Challenge{
-    private String name;
+    private String id;
     private int attempts;
-    private final int max_attempts=3;
-    private Inventory puzzInventory;
-    private HashMap<String, String> questionAnswers = new HashMap<>();
+    private Inventory inventory;
+    private String question, solution;
 
     //Constructor
-    public Puzzle(String name,String description){
+    public Puzzle(String description, String question, String solution){
         super(description);
-        this.setName(name);
         this.attempts=0;
+        this.setQuestion(question);
+        this.setSolution(solution);
     }
 
     //Getters and setters
-    public void setName(String name) {
-        this.name = name;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
     }
 
     public int getAttempts() {
         return attempts;
     }
 
-    public Inventory getPuzzInventory() {
-        return puzzInventory;
+    public Inventory getInventory() {
+        return inventory;//inventory.getItems()
     }
 
     //Methods
-    //initializes the questionAnswers Map of Puzzle with question as the key and solution as the value
-    public void setPuzzle(String puzzleQuestion, String solution) {
-        questionAnswers.put(puzzleQuestion,solution);
-    }
-
-    public void presentPuzzle(){
-        System.out.println(getDescription());
-    }
-
-    public void attemptPuzzle(){
-        List<String> listOfQuestions = new ArrayList<>(questionAnswers.keySet());
-
-        if(getAttempts()<max_attempts){
-            attempts += 1;
-            Scanner myScanner = new Scanner(System.in);
-            System.out.println(listOfQuestions.get(0));
-            String userInput = myScanner.nextLine();
-
-            if(userInput.equalsIgnoreCase(questionAnswers.get(listOfQuestions.get(0)))) {
-                System.out.println("Right answer. Here is your reward");
-                dropAll(); //show the puzzle inventory to the player
-                super.setCleared(true); //set the puzzle flag to be clear
-                listOfQuestions.remove(0);
+    public int attemptPuzzle(String answer){
+            attempts+=1;
+            if(answer.equalsIgnoreCase(getSolution())){
+                super.setCleared(true);
             }
-            else if (getAttempts()<max_attempts){
-                    System.out.println("Sorry wrong answer. Try again\n");
-                    attemptPuzzle();
-                }
-            else{
-                    System.out.println("Wrong Answer!! You have had your chances...You failed...Game Over!!!");
-                    System.exit(0);
-                }
-        }
+            return attempts;
     }
 
-    //displays the puzzles inventory if Puzzle is solved
-    void dropAll(){
-        System.out.println("This will print out the inventory of the puzzle by calling puzzInventory.getItems()");
-        //puzzInventory.getItems();
-    }
-
-    public static void main(String[] args) {
-        Puzzle p1 = new Puzzle("Puzzle1","I'm the formidable puzzle....I dare you solve me." +
-                "\n\nAnswer the below correctly and be rewarded or be ready to meet your fate!\n");
-        p1.setPuzzle("What is 2+2?","4");
-        //p1.getPuzzInventory().addItems(Item item1);
-        p1.presentPuzzle();
-        p1.attemptPuzzle();
-    }
 }
---
+++ src/test/java/com/zombiecastlerush/building/PuzzleTest.java
@@ -0,0 +1,56 @@
+package com.zombiecastlerush.building;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PuzzleTest {
+    Puzzle p1;
+
+    @Before
+    public void setUp() throws Exception {
+        p1 = new Puzzle("Puzzle#1","What is 2+2?","4");
+    }
+
+    @Test
+    public void testSetId_getId() {
+        p1.setId("abc123");
+        assertEquals("abc123",p1.getId());
+    }
+
+    @Test
+    public void testSetQuestion_getQuestion() {
+        String ques ="What is 2+2?";
+        p1.setQuestion(ques);
+        assertEquals(ques,p1.getQuestion());
+    }
+
+    @Test
+    public void testSetSolution_getSolution() {
+        String solution ="4";
+        p1.setSolution(solution);
+        assertEquals(solution,p1.getSolution());
+    }
+
+    @Test
+    public void testgetAttempts() {
+        //when initialized attempts should be zero
+        assertEquals(0,p1.getAttempts());
+    }
+
+    @Test
+    public void testGetInventory() {
+    }
+
+    @Test
+    public void testAttemptPuzzle() {
+        //Player answers incorrectly on first,2nd try
+        assertEquals(1,p1.attemptPuzzle("2"));
+        assertEquals(p1.attemptPuzzle("3"),p1.getAttempts());
+
+        //Player answers correctly on 3rd try
+        assertEquals(3,p1.attemptPuzzle("4"));
+
+
+    }
+}
