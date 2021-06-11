cd6e8246efff2c3b7b04dad67adbdb864149702d
gtulkn12 <gtulkn1@yahoo.com>
2021-06-11 09:42:26 -0400

    AB#93 created combat method

--- src/main/java/com/zombiecastlerush/building/Combat.java
+++ src/main/java/com/zombiecastlerush/building/Combat.java
@@ -1,7 +1,22 @@
 package com.zombiecastlerush.building;
 
+import com.zombiecastlerush.role.Enemy;
+import com.zombiecastlerush.role.Player;
+
 public class Combat extends Challenge{
     public Combat(String description) {
         super(description);
     }
+
+    public static void combat(Player player, Enemy enemy) {
+        while (player.getHealth() > 0 && enemy.getHealth() > 0) {
+            System.out.println("You have entered combat mode, what would you like to do?");
+            System.out.println(" 1 - fight" +
+                    "run-away");
+
+        }
+    }
+
+
+
 }
