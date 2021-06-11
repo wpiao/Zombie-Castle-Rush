0b7fba7056996c9df057739f101cfef68c20c4dd
gtulkn12 <gtulkn1@yahoo.com>
2021-06-11 12:30:55 -0400

    AB#93 created combat choice parser

--- src/main/java/com/zombiecastlerush/building/Combat.java
+++ src/main/java/com/zombiecastlerush/building/Combat.java
@@ -2,29 +2,43 @@ package com.zombiecastlerush.building;
 
 import com.zombiecastlerush.role.Enemy;
 import com.zombiecastlerush.role.Player;
+import com.zombiecastlerush.role.Role;
+import com.zombiecastlerush.util.Parser;
 import com.zombiecastlerush.util.Prompter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Combat extends Challenge {
     public Combat(String description) {
         super(description);
     }
 
-    public static void combat(Player player, Enemy enemy) {
+    public static void combat(Role player, Role enemy) {
 
         while (player.getHealth() > 0 && enemy.getHealth() > 0) {
             String msg = "Welcome to Combat mode, what would you like to do?";
-            String combatDecision = Prompter.getUserInput(msg);
+            String combatChoice = Prompter.getUserInput(msg);
             System.out.println(" 1 - fight" +
                     "run-away");
-            if (combatDecision.isEmpty()) {
+            if (combatChoice.isEmpty()) {
                 continue;
             }
+            combatChoiceParser(combatChoice, player, enemy);
         }
     }
 
-    private static void combatParser(){
-
+    private static void combatChoiceParser(String choice, Role player, Role enemy){
+        List<String> combatCommandList = Parser.parse(choice);
+        if (combatCommandList == null){
+            System.out.println("That's not a valid command. Please try again ....");
+        }
+        else if (combatCommandList.get(0).contains("fight")){
+            fight(player, enemy);
+        }
     }
 
+    private static void fight(Role player, Role enemy) {
 
+    }
 }
