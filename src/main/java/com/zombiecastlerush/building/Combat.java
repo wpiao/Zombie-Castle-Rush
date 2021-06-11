30ffc70165c76e37f786345a1a6affbdd99c829f
gtulkn12 <gtulkn1@yahoo.com>
2021-06-11 10:08:32 -0400

    AB#93 added fight action to Parser ALLOWED_ACTIONS

--- src/main/java/com/zombiecastlerush/building/Combat.java
+++ src/main/java/com/zombiecastlerush/building/Combat.java
@@ -4,17 +4,27 @@ import com.zombiecastlerush.role.Enemy;
 import com.zombiecastlerush.role.Player;
 import com.zombiecastlerush.util.Prompter;
 
-public class Combat extends Challenge{
+public class Combat extends Challenge {
     public Combat(String description) {
         super(description);
     }
 
     public static void combat(Player player, Enemy enemy) {
-        String msg = "Welcome to Combat mode, what would you like to do?";
-        System.out.println(" 1 - fight" +
-                "run-away");
-        String combatDecision = Prompter.getUserInput(msg);
+
         while (player.getHealth() > 0 && enemy.getHealth() > 0) {
+            String msg = "Welcome to Combat mode, what would you like to do?";
+            String combatDecision = Prompter.getUserInput(msg);
+            System.out.println(" 1 - fight" +
+                    "run-away");
+            if (combatDecision.isEmpty()) {
+                continue;
+            }
         }
     }
+
+    private static void combatParser(){
+
+    }
+
+
 }
