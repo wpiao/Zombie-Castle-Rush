428fbb407864fff91b50ef75f9b022930ec32917
gtulkn12 <gtulkn1@yahoo.com>
2021-06-15 00:52:04 -0400

    AB#93 consolidated playerAttack and enemyAttack logic into Combat combat()

--- src/main/java/com/zombiecastlerush/building/Combat.java
+++ src/main/java/com/zombiecastlerush/building/Combat.java
@@ -8,37 +8,35 @@ import java.util.List;
 import java.util.Random;
 
 public class Combat extends Challenge {
+    static int playerDamageToEnemy = new Random().nextInt(50) + 1;
+    static int enemyDamageToPlayer = new Random().nextInt(50) + 1;
+
     public Combat(String description) {
         super(description);
     }
 
     public static void combat(Role player, Role enemy) {
-        playerFight(player, enemy);
 
-    }
-
-    static void playerFight(Role player, Role enemy) {
-        int playerDamageToEnemy = new Random().nextInt(50) + 1;
 
         if (player.getHealth() > 0 && enemy.getHealth() > 0) {
+            playerAttack(player, enemy);
+            enemyAttack(player, enemy);
+        }
+    }
+
+    public static void playerAttack(Role player, Role enemy) {
             System.out.println("You attack...... ");
             enemy.decreaseHealth(playerDamageToEnemy);
             if (enemy.getHealth() < 0) enemy.setHealth(0);
             System.out.println("Enemy sustained " + playerDamageToEnemy + " damage. ");
             System.out.println("Enemy health is now: " + enemy.getHealth());
-            enemyFight(player, enemy);
-        }
     }
 
-    public static void enemyFight(Role player, Role enemy) {
-        int enemyDamageToPlayer = new Random().nextInt(50) + 1;
-
-        if (enemy.getHealth() > 0 && player.getHealth() > 0) {
+    public static void enemyAttack(Role player, Role enemy) {
             System.out.println("Enemy attacks...... ");
             player.decreaseHealth(enemyDamageToPlayer);
             if (player.getHealth() < 0) player.setHealth(0);
             System.out.printf("You sustained %s damage \n", enemyDamageToPlayer);
             System.out.printf("Your health is now: %s \n", player.getHealth());
-        }
     }
 }
