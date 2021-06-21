f7c9465bf0ad934a31bd312599e91d9f7bc90f81
sun624 <67940809+sun624@users.noreply.github.com>
2021-06-21 10:33:35 -0700

    add userInput area for RiddleScreen ab#529

--- src/main/java/com/zombiecastlerush/gui/screens/RiddleScreen.java
+++ src/main/java/com/zombiecastlerush/gui/screens/RiddleScreen.java
@@ -2,6 +2,7 @@ package com.zombiecastlerush.gui.screens;
 
 import asciiPanel.AsciiPanel;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class RiddleScreen implements Screen {
@@ -14,11 +15,27 @@ public class RiddleScreen implements Screen {
     }
 
     public void displayOutput(AsciiPanel terminal) {
+        displayUserInput(terminal,0, terminal.getHeightInCharacters() - 10);
+        terminal.writeCenter(chooseRandomRiddle(), (screenHeight -10) /2 );
 
-        terminal.writeCenter(chooseRandomRiddle(), screenHeight /2 );
     }
 
+    private void displayUserInput(AsciiPanel terminal, int left, int top) {
+        terminal.write(drawLine(terminal.getWidthInCharacters()-1), left, top, Color.orange);
+        terminal.write("Enter command -> ", left, top + 1, Color.red);
+    }
+    private String drawLine(int length) {
+
+        String line = "";
+        for (int i = 0; i < length; i++) {
+            line += "-";
+        }
+        return line;
+    }
+
+
     public Screen respondToUserInput(KeyEvent key) {
+
         return key.getKeyCode() == KeyEvent.VK_ENTER ? new StartScreen() : this;
     }
 
